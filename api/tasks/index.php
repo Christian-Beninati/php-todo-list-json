b6e3773a377c3784da01d286ec882383735fd701
Christian-Beninati <christianbeninati.info@gmail.com>
2023-07-20 17:50:15 +0100

    read-from-database/tasks.json

--- api/tasks/index.php
+++ api/tasks/index.php
@@ -1,6 +1,12 @@
 <?php
+// Indirizzo database
+$database = __DIR__ . '/../../database/tasks.json';
 
-$tasks = ["HTML", "CSS", "Responsive design", "Javascript", "PHP", "Laravel"];
+// Passo l'indirizzo del database 
+$json_data = file_get_contents($database);
+
+// Trasformo in PHP
+$tasks = json_decode($json_data, true);
 
 // Avviso che la risposta e' in Json
 header('Content-Type: application/json');
