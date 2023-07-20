4f66014ef8a6e2fbecb7aad92ae387f4199ffc80
Christian-Beninati <christianbeninati.info@gmail.com>
2023-07-20 18:16:33 +0100

    new_task-API-POST

--- api/tasks/index.php
+++ api/tasks/index.php
@@ -2,11 +2,18 @@
 // Indirizzo database
 $database = __DIR__ . '/../../database/tasks.json';
 
-// Passo l'indirizzo del database 
+// Passo l'indirizzo del database (leggo conenuo JSON)
 $json_data = file_get_contents($database);
 
-// Trasformo in PHP
+// Trasformo in PHP (in array)
 $tasks = json_decode($json_data, true);
+
+
+//  Controlliamo se abbiamo qualcosa in POST
+$new_task = $_POST['task'] ?? null;
+if ($new_task) {
+    $tasks[]  = $new_task;
+}
 
 // Avviso che la risposta e' in Json
 header('Content-Type: application/json');
