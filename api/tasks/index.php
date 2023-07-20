493e8c6e2e53fe181c082b63107543de2cb672c7
Christian-Beninati <christianbeninati.info@gmail.com>
2023-07-20 19:10:27 +0100

    file_put_contents

--- api/tasks/index.php
+++ api/tasks/index.php
@@ -12,7 +12,16 @@ $tasks = json_decode($json_data, true);
 //  Controlliamo se abbiamo qualcosa in POST
 $new_task = $_POST['task'] ?? null;
 if ($new_task) {
+    // Aggiungo new_task a tasks
     $tasks[]  = $new_task;
+
+    // Converto in Json
+    $json_tasks = json_encode($tasks);
+    // Mando a file_put_contents convertito in Json
+    file_put_contents($database, $json_tasks);
+
+    // Avviso che la risposta e' in Json
+    header('Content-Type: application/json');
 }
 
 // Avviso che la risposta e' in Json
