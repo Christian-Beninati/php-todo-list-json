6547084ec1465a41232d5c2f365756c8a59463c3
Christian-Beninati <christianbeninati.info@gmail.com>
2023-07-21 08:16:51 +0100

    test-axios.post

--- my_app/js/script.js
+++ my_app/js/script.js
@@ -11,8 +11,16 @@ const app = createApp ({
     },
     methods: {
         addTask(){
-            console.log(this.newTask);
-            
+            // Preparo i dati
+            const data = {'task' : this.newTask};
+
+            // Preparo le configurazioni
+            const config = {
+                headers: { 'Content-Type': 'x-www-form-urlencoded'}
+            }
+
+            axios.post('http://localhost:8888/php-todo-list-json/api/tasks/' , data , config)
+            .then(res => { console.log('dato inviato, ok') });
         }
     },
     created(){
